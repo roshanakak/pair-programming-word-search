c90faac371371503c5db70a102edb87f32b9f5b0
Roshanak Akbarifar <roshanak.akbarifar@gmail.com>
2021-06-30 20:14:41 +0000

    vertical search added

--- wordsearch.js
+++ wordsearch.js
@@ -3,6 +3,16 @@ const wordSearch = (letters, word) => {
     for (l of horizontalJoin) {
         if (l.includes(word)) return true
     }
+
+    let transposedLetters = letters[0].map((item, colIndex) => letters.map(row => row[colIndex]));
+    const verticalJoin = transposedLetters.map(ls => ls.join(''))
+    for (v of verticalJoin) {
+        if (v.includes(word)) return true;
+    }
+
+return false;
 }
 
 module.exports = wordSearch
+
+
